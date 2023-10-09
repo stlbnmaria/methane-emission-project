3002c4e96abbb99e1adaf97cca48d7b0d7bcdd20
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-09 23:30:38 +0200

    init dataloader with ImageFolder and DataLoader from torch(vision)

---
+++ dataloader.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+import torch
+from torchvision import datasets, transforms
+
+# Define default PATH for data
+PATH = "./data/train_data/images/"
+
+# define transforms
+# in order to load uint16 some transformation with PIL is necessary
+transform = transforms.Compose(
+    [
+        transforms.Lambda(
+            lambda image: torch.from_numpy(
+                np.array(image).astype(np.float32)
+            ).unsqueeze(0)
+        )
+    ]
+)
+
+# create dataset from folder using torchvision
+train_dataset = datasets.ImageFolder(
+    PATH, transform=transform, loader=lambda path: Image.open(path)
+)
+
+# define data loader from torch
+dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
