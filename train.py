8e85c4c3d3f7c42ca66e92c069da010a87efbf2e
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-11 23:03:59 +0200

    commentig, click for params and cleaning

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
+import click
 import logging
-from pathlib import Path
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from torcheval.metrics import BinaryAUROC, BinaryAccuracy, Mean
 from torchvision import models, transforms
-from typing import Tuple
 import time
 import os
 from tempfile import TemporaryDirectory
@@ -16,7 +15,7 @@ from dataloader import load_train_data
 
 
 # create and configure logger
-logging.basicConfig(filename="std.log", format="%(message)s", filemode="w")
+logging.basicConfig(filename="models/modeling.log", format="%(message)s", filemode="w")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -46,16 +45,18 @@ def train_model(
 
             # Each epoch has a training and validation phase
             for phase in ["train", "val"]:
+                # initialise metrics to track
                 auc = BinaryAUROC()
                 acc = BinaryAccuracy(threshold=0.5)
                 running_loss = Mean()
 
+                # set model to correct mode for the phase
                 if phase == "train":
-                    model.train()  # Set model to training mode
+                    model.train()
                 else:
-                    model.eval()  # Set model to evaluate mode
+                    model.eval()
 
-                # Iterate over data.
+                # iterate over batches
                 for inputs, labels in dataloaders[phase]:
                     inputs = inputs.to(device)
                     labels = labels.to(device)
@@ -63,10 +64,10 @@ def train_model(
                     # zero the parameter gradients
                     optimizer.zero_grad()
 
-                    # forward
                     # track history if only in train
                     with torch.set_grad_enabled(phase == "train"):
                         # data augmentation HERE
+                        # TODO: complete and maybe carve out as function
                         if phase == "train":
                             data_aug = nn.Sequential(
                                 transforms.Resize(256),
@@ -87,8 +88,11 @@ def train_model(
                         inputs_aug = data_aug(inputs)
                         #######################
 
+                        # fine tune model - forward pass
                         outputs = model(inputs_aug)
+                        # get probabilities with Softmax activation
                         probs = nn.Softmax(dim=1)(outputs)[:, 1]
+                        # calculate loss
                         loss = criterion(outputs, labels)
 
                         # backward + optimize only if in training phase
@@ -96,17 +100,21 @@ def train_model(
                             loss.backward()
                             optimizer.step()
 
-                    # statistics
+                    # update epoch statistics - meaning add the preds / labels / loss
                     running_loss.update(loss.detach(), weight=len(inputs))
                     auc.update(probs, labels)
                     acc.update(probs, labels)
+
+                # for train set learning rate scheduler
                 if phase == "train":
                     scheduler.step()
 
+                # compute epoch statistics
                 epoch_loss = running_loss.compute().item()
                 epoch_acc = acc.compute().item()
                 epoch_auc = auc.compute().item()
 
+                # log statistics for this epoch
                 logger.info(
                     f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} AUC: {epoch_auc:.4f}"
                 )
@@ -118,6 +126,7 @@ def train_model(
 
             logger.info("")
 
+        # log time for the fold and best validation AUC
         time_elapsed = time.time() - since
         logger.info(
             f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s"
@@ -130,23 +139,29 @@ def train_model(
 
 
 def fine_tune(
-    device: torch.device, dataloaders: dict[str, torch.utils.data.DataLoader], how: str
+    device: torch.device,
+    dataloaders: dict[str, torch.utils.data.DataLoader],
+    how: str,
+    num_epochs: int,
+    learning_rate: float,
 ):
     """ """
+    # if baseline is demanded load simple CNN from models / baseline_cnn.py
     if how == "baseline":
         model_ft = SimpleCNN().to(device)
         logger.info("Initialize baseline model")
+    # if pre-trained load torch pretrained model and replace last layer
     else:
         model_ft = models.resnet18(weights="IMAGENET1K_V1")
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, 2)
-
         model_ft = model_ft.to(device)
 
+    # define loss criterion to be cross entropy
     criterion = nn.CrossEntropyLoss()
 
-    # observe that all parameters are being optimized
-    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    # define optimizer with initial learning rate and momentum
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=learning_rate, momentum=0.9)
 
     # decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
@@ -159,26 +174,33 @@ def fine_tune(
         optimizer_ft,
         exp_lr_scheduler,
         device,
-        num_epochs=5,
+        num_epochs,
     )
     return model_ft, auc
 
 
-def main():
-    folds = 3
+@click.command()
+@click.option("--folds", "-f", default=5, type=int)
+@click.option("--how", "-h", default="pretrained", type=str)
+@click.option("--save", "-s", default=False, type=bool)
+@click.option("--num_epochs", "-e", default=5, type=int)
+@click.option("--learning_rate", "-l", default=0.001, type=float)
+def main(folds, save, how, num_epochs, learning_rate):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    train_data = load_train_data(folds=folds)
 
-    data_path = Path("./data/train_data/images/")
-    train_data = load_train_data(data_path, folds=folds)
+    # count folds and track AUC per fold
     i = 0
     aucs = []
+    # iterate over folds in train_data
     for dataloaders in train_data:
         i += 1
         logger.info("------------------")
         logger.info(f"Starting fold {i}")
-        model_ft, fold_auc = fine_tune(device, dataloaders, how="not-baseline")
+        model_ft, fold_auc = fine_tune(device, dataloaders, how, num_epochs, learning_rate)
         aucs.append(fold_auc)
 
+    logger.info("------------------")
     logger.info(f"Average val AUC: {sum(aucs)/folds:.4f}")
 
 
