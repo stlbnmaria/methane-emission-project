f02ec913d68c062f2c7f73c44b83a3b44375ba82
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-12 01:22:45 +0200

    black formatting

--- EDA/transform_normalization.py
+++ EDA/transform_normalization.py
@@ -6,16 +6,17 @@ from PIL import Image
 
 data_path = Path("./data/train_data/images/")
 transform = transforms.Compose(
-        [
-            transforms.Lambda(
-                lambda image: torch.from_numpy(
-                    # transforms tif to np.float32 
-                    # necessary for pytorch
-                    np.array(image).astype(np.float32) / 65535
-                ).repeat(3, 1, 1)
-            ),
-        ]
-    )
+    [
+        transforms.Lambda(
+            lambda image: torch.from_numpy(
+                # transforms tif to np.float32
+                # necessary for pytorch
+                np.array(image).astype(np.float32)
+                / 65535
+            ).repeat(3, 1, 1)
+        ),
+    ]
+)
 # create dataset from folder using torchvision
 base_dataset = datasets.ImageFolder(
     data_path, transform=transform, loader=lambda path: Image.open(path)
