42a3fc6c67d7e94eb61355bbdc9286a4eafdcf0d
Steve <stevemoses13@gmail.com>
2023-10-12 10:57:31 +0200

    updated Alert function

--- Web_app_scripts/methane_alert.py
+++ Web_app_scripts/methane_alert.py
@@ -1,7 +1,19 @@
 import requests
+from typing import Tuple, Any
 
-# Function to altert methane leakage via email 
-def send_email_via_sendgrid(subject, body, to_email):
+def send_email_via_sendgrid(subject: str, body: str, to_email: str) -> Tuple[int, str]:
+    """
+    Sends an email via the SendGrid API.
+
+    Args:
+    - subject (str): The subject line of the email.
+    - body (str): The main content/body of the email.
+    - to_email (str): The recipient's email address.
+
+    Returns:
+    - Tuple[int, str]: A tuple containing the status code of the API request and the response text.
+    """
+
     SENDGRID_API_URL = "https://api.sendgrid.com/v3/mail/send"
     SENDGRID_API_KEY = "YOUR_SENDGRID_API_KEY"
 
@@ -21,5 +33,6 @@ def send_email_via_sendgrid(subject, body, to_email):
 
     response = requests.post(SENDGRID_API_URL, headers=headers, json=data)
     return response.status_code, response.text
+
 
 send_email_via_sendgrid('Methane Lekage Alert', 'There has been a leakage of methane', 'mosessteve04@gmail.com')
